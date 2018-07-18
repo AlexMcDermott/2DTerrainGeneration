d4d9c43807b40ad2f6e1e53153a028d1b48a9a3b
AlexMcDermott <alexjmcd2202@gmail.com>
2018-07-18 21:04:17 +1000

    Prevent scrolling in IOS safari

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let t;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight).parent(container);
+  createCanvas(windowWidth, windowHeight);
   noStroke();
   colorMode(HSL);
   t = new Terrain(15, 0.005, 2);
