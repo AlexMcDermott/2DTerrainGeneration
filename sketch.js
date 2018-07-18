6c00151110965730380c166def70fbd799fba29c
AlexMcDermott <alexjmcd2202@gmail.com>
2018-07-18 13:23:35 +1000

    Fixed colour issue by switching to HSL

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let t;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-  colorMode(HSB);
+  colorMode(HSL);
   noLoop();
   t = new Terrain(15, 0.005, 2);
 }
--- terrain.js
+++ terrain.js
@@ -8,7 +8,7 @@ class Terrain {
     this.layers = [
       new Layer('Water', 0.0, 0.25, 195, 220),
       new Layer('Sand', 0.25, 0.30, 35, 50),
-      new Layer('Grass', 0.30, 0.55, 80, 130),
+      new Layer('Grass', 0.30, 0.55, 100, 130),
       new Layer('Rock', 0.55, 0.70, 35, 60, true),
       new Layer('Snow', 0.70, 1.0, 70, 95, true),
     ];
